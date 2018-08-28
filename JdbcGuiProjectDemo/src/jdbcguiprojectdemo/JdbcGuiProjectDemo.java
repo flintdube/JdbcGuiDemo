0a0034470b909b8ea72eb7269e1733e5bd22d3e8
flintdube <flintdube@gmail.com>
2018-08-28 16:31:30 +0200

    service and empty model class DB Connection class

---
+++ JdbcGuiProjectDemo/src/jdbcguiprojectdemo/JdbcGuiProjectDemo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jdbcguiprojectdemo;
+
+import java.sql.Connection;
+import jdbcguiprojectdemo.Service.DBconnection;
+/**
+ *
+ * @author Photo.M
+ */
+public class JdbcGuiProjectDemo {
+
+    
+    public static void main(String[] args) {
+       Connection conn = DBconnection.getConnection();
+       
+    }
+    
+}
