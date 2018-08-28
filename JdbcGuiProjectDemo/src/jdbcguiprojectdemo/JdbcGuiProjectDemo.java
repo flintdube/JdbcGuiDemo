b15e24481b70ca12e4254033570143a9e8fe6204
flintdube <flintdube@gmail.com>
2018-08-29 00:10:52 +0200

    Singleton student entity

--- JdbcGuiProjectDemo/src/jdbcguiprojectdemo/JdbcGuiProjectDemo.java
+++ JdbcGuiProjectDemo/src/jdbcguiprojectdemo/JdbcGuiProjectDemo.java
@@ -6,6 +6,8 @@
 package jdbcguiprojectdemo;
 
 import java.sql.Connection;
+import jdbcguiprojectdemo.Model.Student;
+
 import jdbcguiprojectdemo.Service.DBconnection;
 /**
  *
@@ -16,6 +18,9 @@ public class JdbcGuiProjectDemo {
     
     public static void main(String[] args) {
        Connection conn = DBconnection.getConnection();
+       
+       Student student = Student.getInstance();
+        System.out.println(student.toString());
        
     }
     
--- JdbcGuiProjectDemo/src/jdbcguiprojectdemo/Model/Student.java
+++ JdbcGuiProjectDemo/src/jdbcguiprojectdemo/Model/Student.java
@@ -11,4 +11,22 @@ package jdbcguiprojectdemo.Model;
  */
 public class Student {
     
+   int _student_id; 
+   String _firstname;
+   
+   private static Student instance = new Student(101,"Liddon");
+   
+   private Student(int _student_id, String _firstname){
+       this._student_id=_student_id;
+       this._firstname=_firstname;
+   }
+   
+   public static Student getInstance(){
+   return instance;
+   }
+   @Override
+   public String toString(){
+   return String.format("%-5d %-10s", _student_id, _firstname);
+   }
+   
 }
